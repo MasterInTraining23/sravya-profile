1ebb6dc5a24c94abb7bee43614dd5075bf2d7dfd
Amadu Baamba Durham <amadu@amadu-macbookair.roam.corp.google.com>
2017-04-13 21:19:34 -0700

    remove horizontal scrolling and Single Page Application functionality. The site loads each page now.

--- js/tabulation.js
+++ js/tabulation.js
@@ -4,22 +4,7 @@ window.onload = function() {
 	setupSmoothTransitionOnTabSelection();
 	setupChangeTitleCallbackForTabs();
 	localStorage.setItem(currentSectionHeightName, $(window).height() - 85);
-	disableScroll();
 };
-
-// $(window).scroll(function(e) {
-//     var documentScrollTop = $(document).scrollTop();
-//     var totalViewableContent = parseInt(localStorage.getItem(currentSectionHeightName)) + 85;
-//     var heightOfSeenContent = documentScrollTop + $(window).height();
-//     console.log("are we here");
-//     if (heightOfSeenContent > totalViewableContent) {
-//     	console.log("totalViewableContent", totalViewableContent);
-// 	    console.log("heightOfSeenContent", heightOfSeenContent);
-// 	    console.log(e);
-// 	    console.log(documentScrollTop - (heightOfSeenContent - totalViewableContent))
-// 	    $(document).scrollTop(documentScrollTop - (heightOfSeenContent - totalViewableContent));
-//     }
-// });
 
 var SECTION_TABS = 'li.section-tab';
 
@@ -39,57 +24,4 @@ function setupChangeTitleCallbackForTabs() {
 	// data attributes set. The element's 'data-page-title' value
 	// will be used to update the page's title.
 	$(SECTION_TABS).click(function() { document.title = $(this).attr('data-page-title') });
-}
-
-var lastScrollTop = 0;
-
-// down: 40
-var keys = {40: 1};
-
-function preventDefault(e) {
-	console.log("lastScrollTop", lastScrollTop);
-	var documentScrollTop = $(document).scrollTop();
-	console.log("documentScrollTop", documentScrollTop);
-    var totalViewableContent = parseInt(localStorage.getItem(currentSectionHeightName)) + 85;
-    var heightOfSeenContent = documentScrollTop + $(window).height();
-    console.log("are we here");
-    // if (heightOfSeenContent > totalViewableContent) {
-    // 	console.log("totalViewableContent", totalViewableContent);
-	   //  console.log("heightOfSeenContent", heightOfSeenContent);
-	   //  console.log(e);
-	   //  console.log(documentScrollTop - (heightOfSeenContent - totalViewableContent))
-	   //  $(document).scrollTop(documentScrollTop - (heightOfSeenContent - totalViewableContent));
-    // }
-    if (heightOfSeenContent > totalViewableContent) {
-		e = e || window.event;
-		if (e.preventDefault)
-		    e.preventDefault();
-		e.returnValue = false;
-	}
-	lastScrollTop = documentScrollTop;
-}
-
-function preventDefaultForScrollKeys(e) {
-    if (keys[e.keyCode]) {
-        preventDefault(e);
-        return false;
-    }
-}
-
-function disableScroll() {
-  if (window.addEventListener) // older FF
-      window.addEventListener('DOMMouseScroll', preventDefault, false);
-  window.onwheel = preventDefault; // modern standard
-  window.onmousewheel = document.onmousewheel = preventDefault; // older browsers, IE
-  window.ontouchmove  = preventDefault; // mobile
-  document.onkeydown  = preventDefaultForScrollKeys;
-}
-
-function enableScroll() {
-    if (window.removeEventListener)
-        window.removeEventListener('DOMMouseScroll', preventDefault, false);
-    window.onmousewheel = document.onmousewheel = null; 
-    window.onwheel = null; 
-    window.ontouchmove = null;  
-    document.onkeydown = null;  
 }
